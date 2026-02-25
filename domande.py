3aa1eaa36cd00c6c7ec8abe231a6716a06d5f6be
Camilla <johndoe@example.ccamillaferrari04@gmail.com>
2026-02-25 13:53:02 +0100

    Classe domanda

---
+++ domande.py
@@ -0,0 +1,9 @@
+class Domanda:
+    def __init__(self, domanda:str, punteggio:int, giusta:str, sbagliata1:str, sbagliata2:str, sbagliata3:str):
+        self.domanda=domanda
+        self.punteggio=punteggio
+        self.giusta=giusta
+        self.sbagliata1=sbagliata1
+        self.sbagliata2=sbagliata2
+        self.sbagliata3=sbagliata3
+
